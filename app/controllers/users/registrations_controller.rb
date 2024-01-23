cc30b7684b23c3a30c68f26e7d9456dd0e2280b7
fatemanazari <fatemanazari78@gmail.com>
2024-01-23 12:57:56 +0330

    Fix linters

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,20 +1,18 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   private
 
-  def respond_with(resource, options={})
+  def respond_with(resource, _options = {})
     if resource.persisted?
       render json: {
-        status: { code: 200, message: 'Signed up successfully', 
-        data: resource}
-    }, status: :ok
-    else 
+        status: { code: 200, message: 'Signed up successfully',
+                  data: resource }
+      }, status: :ok
+    else
       render json: {
-        status: { message: 'User could not be created successfully', 
-           errors: resource.errors.full_messages }, status: :unprocessable_entity
+        status: { message: 'User could not be created successfully',
+                  errors: resource.errors.full_messages }, status: :unprocessable_entity
       }
     end
   end
