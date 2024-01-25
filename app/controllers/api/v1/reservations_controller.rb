c28db6ce50fc8e6408cf48ff6249e7d22ebe27d4
fatemanazari <fatemanazari78@gmail.com>
2024-01-25 12:12:55 +0330

    feature: add reservation controller

---
+++ app/controllers/api/v1/reservations_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::ReservationsController < ApplicationController
+
+  def index
+    @reservations = current_user.reservations
+    render json: @reservations
+  end
+
+  def create
+    @car = Car.find(params[:car_id])
+    @reservation = @car.reservations.new(reservation_params)
+    @reservation.user = current_user
+
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:date, :location)
+  end
+end
