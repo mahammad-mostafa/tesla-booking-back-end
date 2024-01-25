4d22eda49d95f1dee5dcb1adf59cad7d11bd903e
fatemanazari <fatemanazari78@gmail.com>
2024-01-25 12:16:01 +0330

    Fix rubocop offenses

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::ReservationsController < ApplicationController
-
   def index
     @reservations = current_user.reservations
     render json: @reservations
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,4 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :car, dependent: :destroy
-
-  validates :date, presence: true
-  validates :city, presence: true
 end
