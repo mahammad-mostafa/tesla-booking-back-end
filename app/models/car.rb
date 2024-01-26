cb44dd8c9516c1c8f65db41712177e4d095f44ca
demesameneshoa <demesameneshoa@gmail.com>
2024-01-26 23:08:19 +0300

    feat: add car and reservations association, and performance details nested attribute

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :performance_details, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+  
+  accepts_nested_attributes_for :performance_details, allow_destroy: true
 end
