232015c990096e5af87b57da49f4a2be7c1fb202
demesameneshoa <demesameneshoa@gmail.com>
2024-01-28 21:28:10 +0300

    feat: add validations to car model|

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,4 +4,9 @@ class Car < ApplicationRecord
   has_many :reservations, dependent: :destroy
 
   accepts_nested_attributes_for :performance_details, allow_destroy: true
+
+  validates :model_name, presence: true
+  validates :image, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
+  validates :description, presence: true
+  validates :rental_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
